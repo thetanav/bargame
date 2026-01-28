add0b368732f112885280e28147234d3c0acaf3e
Tanav Poswal <tanavposwal939@gmail.com>
2026-01-28 22:18:29 +0530

    add anlytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { NuqsAdapter } from 'nuqs/adapters/react'
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <NuqsAdapter>
       <App />
+      <Analytics />
     </NuqsAdapter>
   </StrictMode>,
 )
